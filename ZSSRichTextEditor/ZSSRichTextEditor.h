053233c30b6859d12004968876f6c99077ec4fcc
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-01-22 12:41:56 +0500

    fix & fix variables

--- ZSSRichTextEditor/ZSSRichTextEditor.h
+++ ZSSRichTextEditor/ZSSRichTextEditor.h
@@ -5,6 +5,7 @@
 //  Created by Nicholas Hubbard on 11/30/13.
 //  Copyright (c) 2013 Zed Said Studio. All rights reserved.
 //
+//
 
 #import <UIKit/UIKit.h>
 #import "HRColorPickerViewController.h"
