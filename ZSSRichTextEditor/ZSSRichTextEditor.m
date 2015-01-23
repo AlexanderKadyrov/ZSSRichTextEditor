4851c4ad6d94a3b769fedc8c8663dc6d39d70c80
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-01-23 13:37:34 +0500

    fix

--- ZSSRichTextEditor/ZSSRichTextEditor.m
+++ ZSSRichTextEditor/ZSSRichTextEditor.m
@@ -125,7 +125,7 @@ static Class hackishFixClass = Nil;
     self.editorView.dataDetectorTypes = UIDataDetectorTypeNone;
     self.editorView.scrollView.bounces = NO;
     self.editorView.backgroundColor = [UIColor whiteColor];
-    [self.editorView.scrollView setContentSize:CGSizeMake(w, h)];
+    //[self.editorView.scrollView setContentSize:CGSizeMake(w, h)];
     [self.mainView addSubview:self.editorView];
     
     if (!self.resourcesLoaded) {
@@ -1134,7 +1134,7 @@ static Class hackishFixClass = Nil;
     //CGFloat contentSiseHeight = self.editorView.scrollView.contentSize.height;
     //self.editorView.scrollView.contentSize = CGSizeMake(self.editorView.frame.size.width, contentSiseHeight + 20);
     
-    //[self.editorView.scrollView setContentOffset:CGPointMake(0, contentSiseHeight + 10) animated:NO];
+    [self.editorView.scrollView setContentOffset:CGPointMake(0, self.editorView.frame.size.height) animated:NO];
 }
 
 
