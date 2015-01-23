b585df0820184207438dea1a972f791159cb8ec8
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-01-23 19:13:46 +0500

    fix

--- ZSSRichTextEditor/ZSSRichTextEditor.m
+++ ZSSRichTextEditor/ZSSRichTextEditor.m
@@ -1123,6 +1123,10 @@ static Class hackishFixClass = Nil;
             [self blurTextEditor];
         });
     }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.editorView.scrollView.contentOffset = CGPointMake(0, -(self.editorView.scrollView.contentInset.top));
+    });
 }
 
 
