fd786ef4f9ff3a9e49d5403ada5ad65203d1c26c
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-01-26 11:13:17 +0500

    fix

--- ZSSRichTextEditor/ZSSRichTextEditor.m
+++ ZSSRichTextEditor/ZSSRichTextEditor.m
@@ -116,7 +116,10 @@ static Class hackishFixClass = Nil;
     
     // Editor View
     self.editorView = [[UIWebView alloc] initWithFrame:CGRectMake(x, y, w, h)];
+    
     self.editorView.delegate = self;
+    self.editorView.scrollView.delegate = self;
+    
     self.editorView.hidesInputAccessoryView = YES;
     self.editorView.keyboardDisplayRequiresUserAction = NO;
     self.editorView.scalesPageToFit = YES;
