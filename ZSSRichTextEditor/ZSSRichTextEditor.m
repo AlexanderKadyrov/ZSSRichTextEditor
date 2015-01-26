60a0299ec2fcbf617a91ecbc3fa5ec5b722f1a62
Kadyrov Alexander <alexander-kad@yandex.ru>
2015-01-26 17:03:02 +0500

    fix

--- ZSSRichTextEditor/ZSSRichTextEditor.m
+++ ZSSRichTextEditor/ZSSRichTextEditor.m
@@ -161,11 +161,11 @@ static Class hackishFixClass = Nil;
     }
      
     //make Toolbar
-    UIToolbar *backgroundToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(8, 8, width, 44)];
+    UIToolbar *backgroundToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, width, 44)];
     backgroundToolbar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     
     // Scrolling View
-    self.toolBarScroll = [[UIScrollView alloc] initWithFrame:CGRectMake(8, 8, width, 44)];
+    self.toolBarScroll = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, width, 44)];
     self.toolBarScroll.backgroundColor = [UIColor clearColor];
     self.toolBarScroll.showsHorizontalScrollIndicator = NO;
     
@@ -178,7 +178,7 @@ static Class hackishFixClass = Nil;
     self.toolBarScroll.autoresizingMask = self.toolbar.autoresizingMask;
     
     // Parent holding view
-    self.toolbarHolder = [[UIView alloc] initWithFrame:CGRectMake(8, 8, width, 44)];
+    self.toolbarHolder = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, 44)];
     self.toolbarHolder.autoresizingMask = self.toolbar.autoresizingMask;
     [self.toolbarHolder addSubview:self.toolBarScroll];
     [self.toolbarHolder insertSubview:backgroundToolbar atIndex:0];
